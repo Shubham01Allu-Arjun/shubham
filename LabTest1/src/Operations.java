5284d27bab41839f05991981c9ebcc64d1120a44
Anudip <Anudip@DESKTOP-S6B7GOG>
2022-09-08 12:25:13 +0530

    First

---
+++ LabTest1/src/Operations.java
@@ -0,0 +1,24 @@
+
+public class Operations {
+
+	public static void main(String[] args)
+	{
+		int a=5;
+		int b=6;
+		String cha="+";
+		switch(cha)
+		{
+		case "+":System.out.println(a+b);
+		break;
+		case "*":System.out.println(a*b);
+		break;
+		case "-":System.out.println(a-b);
+		break;
+		case "/":System.out.println(a/b);
+		break;
+		default: System.out.println("wrong input");
+		}				
+		
+	}
+
+}
