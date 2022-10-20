4459d6f78d87608db74204c5c18d41d444f60c49
Shubham@23 <111141096+Shubham01Allu-Arjun@users.noreply.github.com>
2022-10-20 12:49:53 +0530

    Add files via upload

---
+++ LabTest 7 MultiThread/src/com/Multithread/MyThread.java
@@ -0,0 +1,22 @@
+package com.Multithread;
+
+
+	public class MyThread extends Thread {
+	    public void run() {
+	        String[] friends = {"Amar", "Akbar", "Anthony", "David ", "Emiway", "virat", "George", "Henry", "Shubham", "Jack"};
+	        for (String friend : friends) {
+	            System.out.println(friend);
+	        }
+	   }
+
+	// Main Method 
+	    public static void main(String[] args) {
+	        MyThread firstThread = new MyThread();
+	        MyThread secondThread = new MyThread();
+	        MyThread thirdThread = new MyThread();
+	        firstThread.start();
+	        secondThread.start();
+	        thirdThread.start();
+	    }
+	}
+
---
+++ LabTest 7 MultiThread/src/com/Multithread/Producer.java
@@ -0,0 +1,28 @@
+package com.Multithread;
+
+
+	public class Producer {
+		class ProdCons{
+			int availableProduct=0;
+			
+			public synchronized void demand(int n){
+				System.out.println("Demanding for "+n);
+				if(availableProduct<n){
+					System.out.println("Not enough products available");
+					 try{  
+						wait();  
+					}catch(Exception e){System.out.println(e);}  
+				}
+				availableProduct=availableProduct-n;
+				System.out.println("Available Product: "+availableProduct);
+			}
+			
+			public synchronized void supply(int n){
+				System.out.println("Supplying "+n);
+				availableProduct=availableProduct+n;
+				System.out.println("Available Product: "+availableProduct);
+				notify();  
+			}
+		}
+	}
+
